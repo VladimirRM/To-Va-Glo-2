d9b16c6a7701b7f1436b39d6583f3520a1e3ee0e
Vladimir RM <shvdrummer@gmail.com>
2022-11-13 11:15:59 +0200

    change connect with html

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const input = document.querySelector('#input')
+const btn = document.querySelector('#btn')
+const result = document.querySelector('#input')
+const total = document.querySelector('#input')
